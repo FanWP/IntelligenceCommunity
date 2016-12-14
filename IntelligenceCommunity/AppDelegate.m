46751a57ff931c680cf4d4d02d12b99c25d72f92
FanWP <1215630349@qq.com>
2016-12-14 14:18:04 +0800

    321

--- IntelligenceCommunity/AppDelegate.m
+++ IntelligenceCommunity/AppDelegate.m
@@ -20,6 +20,10 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     
+    
+    
+    ///
+    
     [self initUI];
     
     
