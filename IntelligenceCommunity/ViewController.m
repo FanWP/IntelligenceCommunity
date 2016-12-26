dc294fa897f816da0dcf0432e350b12f5fddd42a
mumu <2435324075@qq.com>
2016-12-26 11:03:33 +0800

    test2

--- IntelligenceCommunity/ViewController.m
+++ IntelligenceCommunity/ViewController.m
@@ -24,6 +24,8 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
     // Dispose of any resources that can be recreated.
+    // Dispose of any resources that can be recreated.
+
 
 }
 
